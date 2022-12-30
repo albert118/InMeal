1673e211a93e434b236fb87f8f107e87550d5c4f
theQuantumMechanic <albertferguson118@gmail.com>
2022-12-30 23:15:43 +1100

    feature: add the migration infrastructure
    Signed-off-by: theQuantumMechanic <albertferguson118@gmail.com>

---
+++ src/InMeal/InMeal.Core/InMealDbMigrationContext.cs
@@ -0,0 +1,28 @@
+﻿using InMeal.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace InMeal.Core;
+
+public class InMealDbMigrationContext : DbContext
+{
+    public InMealDbMigrationContext(DbContextOptions opts) : base(opts) { }
+
+    public DbSet<Ingredient> Ingredients => Set<Ingredient>();
+
+    public DbSet<Recipe> Recipes => Set<Recipe>();
+
+    public DbSet<RecipeIngredient> RecipeIngredients => Set<RecipeIngredient>();
+
+    public DbSet<RecipePhoto> RecipePhotos => Set<RecipePhoto>();
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        new IngredientConfig().Configure(builder.Entity<Ingredient>());
+
+        new RecipeConfig().Configure(builder.Entity<Recipe>());
+
+        new RecipeIngredientConfig().Configure(builder.Entity<RecipeIngredient>());
+
+        new RecipePhotoConfig().Configure(builder.Entity<RecipePhoto>());
+    }
+}
---
+++ src/InMeal/InMeal.Core/InMealDbMigrationContextFactory.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace InMeal.Core;
+
+public class InMealDbMigrationContextFactory : IDesignTimeDbContextFactory<InMealDbMigrationContext>
+{
+    // Holds migration infrastructure settings
+    private const string AppSettingsFilePath = "appsettings.json";
+
+    // configuration for a JSON settings file will not work without
+    // this package installed 'Microsoft.Extensions.Configuration.Json'
+
+    public InMealDbMigrationContext CreateDbContext(string[] args)
+    {
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile(AppSettingsFilePath)
+            .Build();
+
+        var connectionString = configuration.GetConnectionString("InMealDbConnection");
+
+        Console.WriteLine(Directory.GetCurrentDirectory().ToString());
+
+        var majorVersion = int.Parse(configuration.GetSection("ConnectionStrings:ServerVersionMajor").Value);
+        var minorVersion = int.Parse(configuration.GetSection("ConnectionStrings:ServerVersionMinor").Value);
+        var buildVersion = int.Parse(configuration.GetSection("ConnectionStrings:ServerVersionBuild").Value);
+
+        var serverVersion = new MySqlServerVersion(new Version(majorVersion, minorVersion, buildVersion));
+
+        var dbContextBuilder =
+            new DbContextOptionsBuilder<InMealDbMigrationContext>().UseMySql(connectionString, serverVersion);
+
+        return new(dbContextBuilder.Options);
+    }
+}
