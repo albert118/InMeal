ccc3cc290933119eb2274f93bff3bc5bc964e22f
theQuantumMechanic <albertferguson118@gmail.com>
2022-12-30 21:28:09 +1100

    bootstap: edit the template, add service config and Startup.cs
    Signed-off-by: theQuantumMechanic <albertferguson118@gmail.com>

--- src/InMeal/InMeal/Program.cs
+++ src/InMeal/InMeal/Program.cs
@@ -1,24 +1,14 @@
+using InMeal;
+
 var builder = WebApplication.CreateBuilder(args);
+var startup = new Startup(builder.Configuration);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+// Do not configure or register the container services here! Instead edit Startup.ConfigureServices
+startup.ConfigureServices(builder.Services);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment()) {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
+// Do not configure the HTTP request pipeline in this Program entrypoint instead edit Startup.Configure
+startup.Configure(app, builder.Environment);
 
 app.Run();
---
+++ src/InMeal/InMeal/Startup.cs
@@ -0,0 +1,46 @@
+﻿namespace InMeal;
+
+public class Startup
+{
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    /// <summary>
+    /// Add and configure services for the container
+    /// </summary>
+    /// <param name="services"></param>
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers();
+
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+    }
+
+    /// <summary>
+    /// Configure the webapplication depending on the environment
+    /// </summary>
+    public void Configure(WebApplication app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment()) {
+            app.UseSwagger()
+                .UseSwaggerUI()
+                .UseDeveloperExceptionPage();
+        }
+        else {
+            // Enable the exception handler route
+            app.UseExceptionHandler("/error")
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                .UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.MapControllers();
+    }
+}
