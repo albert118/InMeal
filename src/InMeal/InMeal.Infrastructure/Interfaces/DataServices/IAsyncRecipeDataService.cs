f6710ef6c9fb660710c085d243abc7d5892850fc
theQuantumMechanic <albertferguson118@gmail.com>
2022-12-31 13:36:00 +1100

    feature: added the data service
    Signed-off-by: theQuantumMechanic <albertferguson118@gmail.com>

---
+++ src/InMeal/InMeal.Infrastructure/Interfaces/DataServices/IAsyncRecipeDataService.cs
@@ -0,0 +1,17 @@
+﻿using InMeal.Core.Entities;
+
+namespace InMeal.Infrastructure.Interfaces.DataServices;
+
+public interface IAsyncRecipeDataService
+{
+    Task<Guid?> AddRecipeAsync(string? title, string? blurb, string? prepSteps, int? cookTime, int? prepTime,
+        CancellationToken ct);
+
+    Task<List<Recipe>> GetRecipesAsync(ICollection<Guid> guids, CancellationToken ct);
+
+    Task<Recipe?> GetRecipeAsync(Guid id, CancellationToken ct);
+
+    // A dumb update method
+    Task<bool> EditRecipeAsync(Guid id, string title, string? blurb, string prepSteps, int? cookTime, int? prepTime,
+        CancellationToken ct);
+}
