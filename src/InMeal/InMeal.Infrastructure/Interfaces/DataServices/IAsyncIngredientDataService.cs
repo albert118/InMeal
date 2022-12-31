6f00a45c22639c873ab0755af3d4e053514e734d
theQuantumMechanic <albertferguson118@gmail.com>
2022-12-31 14:30:02 +1100

    feature: ingredient dataservice
    Signed-off-by: theQuantumMechanic <albertferguson118@gmail.com>

---
+++ src/InMeal/InMeal.Infrastructure/Interfaces/DataServices/IAsyncIngredientDataService.cs
@@ -0,0 +1,10 @@
+﻿using InMeal.Core.Entities;
+
+namespace InMeal.Infrastructure.Interfaces.DataServices;
+
+public interface IAsyncIngredientDataService
+{
+    Task<List<Ingredient>> GetIngredientsAsync(ICollection<Guid> ids, CancellationToken ct);
+
+    Task<bool> AddIngredientsAsync(ICollection<string> names, CancellationToken ct);
+}
