3aa131ef87fcb1903005b02bfe01f99e10de7c64
theQuantumMechanic <albertferguson118@gmail.com>
2023-01-03 20:18:54 +1100

    feature: sidebar tweaked and icons added

--- src/meal-ui/src/components/MinimalistSidebar/MinimalistSidebar.js
+++ src/meal-ui/src/components/MinimalistSidebar/MinimalistSidebar.js
@@ -1,17 +1,29 @@
 import React from 'react'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { faBars, faCalendar, faBoxes, faGear } from '@fortawesome/free-solid-svg-icons'
 
 export default function MinimalistSidebar() {
     return (
         <div className="minimalist-sidebar">
-            <button className="sidebar-toggle-btn" type="button">
-                <FontAwesomeIcon icon={faBars} />
-            </button>
-            <p>#1</p>
-            <p>#2</p>
-            <p>#3</p>
+            <div className="top">
+                <button className="btn primary-btn" type="button">
+                    <FontAwesomeIcon icon={faBars} />
+                </button>
+                <button className="btn" type="button">
+                    <FontAwesomeIcon icon={faCalendar} />
+                </button>
+                <button className="btn" type="button">
+                    <FontAwesomeIcon icon={faBoxes} />
+                </button>
+            </div>
+
+
+            <div className="bottom">
+                <button className="btn" type="button">
+                    <FontAwesomeIcon icon={faGear} />
+                </button>
+            </div>
         </div>
     );
 };
