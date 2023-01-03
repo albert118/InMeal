15632721e7a641873ae4a35c4ffdbebbd1f55303
theQuantumMechanic <albertferguson118@gmail.com>
2023-01-03 19:57:10 +1100

    feature: add negative spacing constants

--- src/meal-ui/src/App.js
+++ src/meal-ui/src/App.js
@@ -1,12 +1,12 @@
 import 'styles/styles.css'; 
 
-import RouterConfig from 'navigation/RouterConfig';
+import RouterConfig from './navigation/RouterConfig';
 import { BrowserRouter } from 'react-router-dom';
 
 
 export default function App() {
   return (
-    <div className="meal-ui-basekit-theme">
+    <div className="meal-ui-basekit-theme negative-space-scale">
       <BrowserRouter>
           <RouterConfig />
       </BrowserRouter>
