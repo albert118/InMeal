eada9bb411a5bb083bf6af1575a62e48d7f7e42d
theQuantumMechanic <albertferguson118@gmail.com>
2023-01-07 13:37:45 +1100

    refactor: drop the old input styles

--- src/meal-ui/src/forms/Inputs/Checkbox.js
+++ src/meal-ui/src/forms/Inputs/Checkbox.js
@@ -1,11 +1,12 @@
 import React from "react";
 
-/// label should be unique
+
+
 const Checkbox = props => {
     const { name, label, value } = props
 
     return (
-        <div className="form-input">
+        <div className="form-input u-form-flexed-input">
             <input type="checkbox" id={label} value={value} name={name} />
             <label htmlFor={label}>{label}</label>
         </div>
