a07d4bb763d8ed80e971c98c2b9f31e739ea7b3a
theQuantumMechanic <albertferguson118@gmail.com>
2023-01-07 12:15:08 +1100

    feature: added forms
    * adds colours, basic styles
    * adds some text inputs
    *adds the generic form context

---
+++ src/meal-ui/src/forms/Inputs/LongTextInput.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+
+const LongTextInput = props => {
+    const { label, value, placeholder, handler } = props;
+
+    return(
+        <div className="form-input">
+            <label for={label}>{label}</label>
+            <textarea 
+                id={label} 
+                className="long-text-input"
+                name={label} 
+                value={value}
+                placeholder={placeholder} 
+                onChange={handler}
+            />
+        </div>
+    );
+};
+
+export { LongTextInput };
---
+++ src/meal-ui/src/forms/Inputs/TextInput.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const TextInput = props => {
+    const { label, value, placeholder, handler } = props;
+
+    return(
+        <div className="form-input">
+            <label for={label}>{label}</label>
+            <input 
+                id={label} 
+                className="text-input"
+                key={label} 
+                value={value}
+                placeholder={placeholder} 
+                onChange={handler}
+                type="text" 
+            />
+        </div>
+    );
+};
+
+
+export { TextInput };
