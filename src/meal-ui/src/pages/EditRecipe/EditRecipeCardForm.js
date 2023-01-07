be8e73ba50c8c32d842dc0e4159ca93f126c66a7
theQuantumMechanic <albertferguson118@gmail.com>
2023-01-07 13:17:38 +1100

    feature: added a nicer scrollbar

--- src/meal-ui/src/pages/EditRecipe/EditRecipeCardForm.js
+++ src/meal-ui/src/pages/EditRecipe/EditRecipeCardForm.js
@@ -10,9 +10,9 @@ const EditRecipeCardForm = props => {
 
     const [recipe, setRecipe] = useState({
         id: recipeId,
-        title: "",
+        title: '',
         status: "unsaved",
-        blurb: "add some details",
+        blurb: '',
         recipeIngredients: [],
         preparationSteps: [],
         image: {
@@ -21,19 +21,31 @@ const EditRecipeCardForm = props => {
         }
     });
 
-    const handleCancel = () => {
+    const handleChange = (event) => {
+        setRecipe({ ...recipe, [event.target.name]: event.target.value });
+    };
+
+    const handleCancel = event => {
+        event.preventDefault();
         console.log("cancelled");
-        return null;
+        setRecipe({
+            id: recipeId,
+            title: '',
+            status: "unsaved",
+            blurb: '',
+            recipeIngredients: [],
+            preparationSteps: [],
+            image: {
+                label: null,
+                url: "https://64.media.tumblr.com/2b34471a440e97cd99f5728954238b3f/c4e6a303827cff2d-07/s540x810/fd32c1315bdfc4271b125bd417c999d4abb18126.gif"
+            }
+        });
     };
 
-    const handleSave = () => {
-        console.log("saved");
-        return null;
-    };
-
-    const handleEditTitle = newTitle => {
-        console.log(newTitle);
-        return null;
+    const handleSave = event => {
+        // event.preventDefault();
+        console.log("saved - now cancelling for demo");
+        handleCancel(event);
     };
 
     return(
@@ -45,11 +57,11 @@ const EditRecipeCardForm = props => {
             </div>
 
             <TitleBar>
-                <TextInput label={recipe.title} value={recipe.title} placeholder="add a new title" handler={handleEditTitle} />
+                <TextInput name={"title"} value={recipe.title} placeholder="add a new title" handler={handleChange} />
             </TitleBar>
 
             <div className="recipe-data-slot recipe-content-grid">
-                <p className="recipe-content-blurb">{recipe.blurb}</p>
+                <LongTextInput className="recipe-content-blurb" name={"blurb"} value={recipe.blurb} placeholder="add some details" handler={handleChange} />
                 <div className="recipe-content-ingredients">
                     {/* { recipe.recipeIngredients.map(ingredient => 
                         <Checkbox label={ingredient.label} value={false} />
