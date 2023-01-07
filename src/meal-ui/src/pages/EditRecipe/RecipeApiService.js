7261289b5762bd8c624ef5490a6f28803cd0f645
theQuantumMechanic <albertferguson118@gmail.com>
2023-01-07 16:10:45 +1100

    package: use fetch-hook instead
    * replace fetch pacakage

---
+++ src/meal-ui/src/pages/EditRecipe/RecipeApiService.js
@@ -0,0 +1,15 @@
+import React from "react";
+import useFetch from "react-fetch-hook";
+
+const RecipeApiService = () => {
+    const post = recipe => {
+
+    };
+
+    return {
+
+    };
+};
+
+
+export default RecipeApiService;
