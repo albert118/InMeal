9b39bed90cf38feb5bb9686eadf149682ce91ba4
theQuantumMechanic <albertferguson118@gmail.com>
2023-01-08 15:01:27 +1100

    housekeeping: drop dead dummy data

--- src/meal-ui/src/pages/EditRecipe/EditRecipeCardForm.js
+++ src/meal-ui/src/pages/EditRecipe/EditRecipeCardForm.js
@@ -30,22 +30,6 @@ const getStartingRecipe = recipeId => {
     };
 };
 
-const getDefaultRecipe = recipeId => {
-    return {
-        id: recipeId,
-        title: '',
-        status: FormStatuses.Unsaved,
-        blurb: '',
-        recipeIngredients: [],
-        prepSteps: [],
-        image: {
-            label: null,
-            url: "https://64.media.tumblr.com/2b34471a440e97cd99f5728954238b3f/c4e6a303827cff2d-07/s540x810/fd32c1315bdfc4271b125bd417c999d4abb18126.gif"
-        }
-    };
-};
-
-
 const EditRecipeCardForm = props => {
     const { recipeId } = props;
     
