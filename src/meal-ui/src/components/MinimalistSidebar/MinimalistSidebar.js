059b2ab6ee780199a00dea0757579ab1b9931e9a
theQuantumMechanic <albertferguson118@gmail.com>
2023-01-17 02:13:18 +1100

    revert "uncommitted changes" (#10)

--- src/meal-ui/src/components/MinimalistSidebar/MinimalistSidebar.js
+++ src/meal-ui/src/components/MinimalistSidebar/MinimalistSidebar.js
@@ -15,11 +15,17 @@ export default function MinimalistSidebar() {
     const handlePlanningClick = () => navigate(AppRoutes.planning);
     const handleSettingsClick = () => navigate(AppRoutes.settings);
 
+    // the initial state is falsy inactive, the animation begins after the first click
     const [isActive, setActive] = useState(false);
-    const toggleActive = () => setActive(!isActive);
+    const [isInActive, setInActive] = useState(null);
+
+    const toggleActive = () => {
+        setActive(!isActive);
+        setInActive(isActive);
+    };
 
     return (
-        <div className={`minimalist-sidebar ${isActive ? "minimalist-sidebar-toggle": ""}`}>
+        <div className={`minimalist-sidebar ${isActive ? "minimalist-sidebar-active" : ""} ${isInActive ? "minimalist-sidebar-inactive" : ""}`}>
             <div className="hero-branding-logo">
                 <button type="button" onClick={handleDashboardClick}>
                     <h1 className="hero-title">{config.BrandName}</h1>
