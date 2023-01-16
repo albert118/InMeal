c045f8401f72aadc344ce304721587873dd692c1
theQuantumMechanic <albertferguson118@gmail.com>
2023-01-17 00:58:24 +1100

    feature: animated burger

--- src/meal-ui/src/components/MinimalistSidebar/AnimatedHamburger.js
+++ src/meal-ui/src/components/MinimalistSidebar/AnimatedHamburger.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export const AnimatedHamburger = props => {
     const { callback } = props;
 
+    const [isActive, setActive] = useState(false);
+
+    const handleClick = event => {
+        event.preventDefault();
+        setActive(!isActive);
+        callback();
+    };
+
     return (
-        <div className='hamburger' onClick={callback}>
+        <div className={`hamburger ${isActive ? "hamburger-toggle": ""}`} onClick={handleClick}>
             <div className='line1'></div>
             <div className='line2'></div>
             <div className='line3'></div>
