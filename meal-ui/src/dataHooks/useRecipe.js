0a6935a3d2ec29814c7bb92ac08296680b23e1e2
theQuantumMechanic <albertferguson118@gmail.com>
2023-02-24 15:25:09 +1100

    useRecipe data service expanded

--- meal-ui/src/dataHooks/useRecipe.js
+++ meal-ui/src/dataHooks/useRecipe.js
@@ -1,26 +1,63 @@
 import { useEffect, useState } from 'react';
 import defaultRequestOptions from './defaultRequestOptions';
+import { ApiConfig } from 'config';
+
+const getRecipe = async id => {
+	const url = `${ApiConfig.API_URL}/recipe?id=${encodeURIComponent(id)}`;
+
+	const response = await fetch(url, {
+		...defaultRequestOptions,
+		method: 'GET'
+	});
+
+	const data = await response.json();
+
+	return data;
+};
+
+const patchRecipe = async (recipe, ingredients, preparationSteps) => {
+	const url = `${ApiConfig.API_URL}/recipe`;
+
+	const response = await fetch(url, {
+		...defaultRequestOptions,
+		method: 'PATCH',
+		body: JSON.stringify({
+			...recipe,
+			recipeIngredientDtos: ingredients,
+			prepSteps: preparationSteps
+		})
+	});
+
+	return response;
+};
+
+const putIngredient = async ingredientName => {
+	const url = `${
+		ApiConfig.API_URL
+	}/ingredient?newIngredientName=${encodeURIComponent(ingredientName)}`;
+
+	const response = await fetch(url, {
+		...defaultRequestOptions,
+		method: 'PUT'
+	});
+
+	const data = await response.json();
+
+	return data;
+};
 
 export default function useRecipe(recipeId) {
 	const [recipe, setRecipe] = useState(null);
 	const [isLoading, toggleLoading] = useState(true);
 
 	useEffect(() => {
-		const loadRecipe = async () => {
-			const url = `https://localhost:7078/api/recipe?id=${encodeURIComponent(
-				recipeId
-			)}`;
-
-			const data = await (
-				await fetch(url, { ...defaultRequestOptions, method: 'GET' })
-			).json();
-
+		getRecipe(recipeId).then(data => {
 			setRecipe(data);
 			toggleLoading(false);
-		};
-
-		loadRecipe();
+		});
 	}, []);
 
 	return { recipe, isLoading };
 }
+
+export { getRecipe, patchRecipe, putIngredient };
