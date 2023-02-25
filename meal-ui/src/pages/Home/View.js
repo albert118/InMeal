1a054f4ab8fa92aacf4f08eaa887b3c79885c892
theQuantumMechanic <albertferguson118@gmail.com>
2023-02-25 22:15:14 +1100

    included simple card on homepage
    * adds action links with a header, label, and button

--- meal-ui/src/pages/Home/View.js
+++ meal-ui/src/pages/Home/View.js
@@ -1,26 +1,67 @@
-import React, { useContext } from "react";
-import Card from 'components/Card';
+import { useContext } from 'react';
+import Card, { SimpleCard } from 'components/Card';
 import Carousel from 'components/Carousel';
 import { GenericContext } from 'pages/GenericPageContainer';
-
+import Button from 'components/Button';
+import { useNavigate } from 'react-router-dom';
+import AppRoutes from 'navigation/AppRoutes';
 
 export default function View(props) {
-    const { plannedItems, suggestedItems } = props;
+	const { plannedItems, suggestedItems } = props;
 
-    const genericContext = useContext(GenericContext);
+	const genericContext = useContext(GenericContext);
 
-    const classes = genericContext.className 
-        ? `p-home-view ${genericContext.className}` 
-        : `p-home-view`;
+	const navigate = useNavigate();
 
-    return (
-        <div className={classes}>
-            <Card className="planning-quick-view" title="Upcoming...">
-                <Carousel items={plannedItems}/>
-            </Card>
-            <Card className="explore-quick-view" title="Something else?">
-                <Carousel items={suggestedItems}/>
-            </Card>
-        </div>
-    );
-};
+	const classes = genericContext.className
+		? `p-home-view ${genericContext.className}`
+		: `p-home-view`;
+
+	return (
+		<div className={classes}>
+			<Card
+				className='planning-quick-view'
+				title='Upcoming...'
+			>
+				<Carousel items={plannedItems} />
+			</Card>
+			<Card
+				className='explore-quick-view'
+				title='Something else?'
+			>
+				<Carousel items={suggestedItems} />
+			</Card>
+			<SimpleCard>
+				<div>
+					Recipes
+					<div className='action-option'>
+						Add a new recipe
+						<Button
+							handler={() => navigate(`${AppRoutes.recipe}/add`)}
+						>
+							Add
+						</Button>
+					</div>
+				</div>
+				<div>
+					Pantry
+					<div className='action-option'>
+						Update the pantry
+						<Button handler={() => navigate(`${AppRoutes.root}`)}>
+							add ingredients
+						</Button>
+					</div>
+				</div>
+				<div>
+					Planning
+					<div className='action-option'>
+						Create a meal plan
+						<Button handler={() => navigate(`${AppRoutes.root}`)}>
+							plan
+						</Button>
+					</div>
+				</div>
+			</SimpleCard>
+		</div>
+	);
+}
