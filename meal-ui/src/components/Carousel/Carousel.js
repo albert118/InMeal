2ebefd7c72b1a1903c78789c47ebe29321a54db3
theQuantumMechanic <albertferguson118@gmail.com>
2023-02-25 22:42:04 +1100

    refactored home page

--- meal-ui/src/components/Carousel/Carousel.js
+++ meal-ui/src/components/Carousel/Carousel.js
@@ -1,45 +1,56 @@
-import React from "react";
+import React from 'react';
 import { Splide, SplideSlide } from '@splidejs/react-splide';
-import { ImageCard } from "components/Card";
+import { ImageCard } from 'components/Card';
 import '@splidejs/react-splide/css';
 
-
 // An item is expected in the structure
-// item = { 
+// item = {
 //      id: "unique string ID",
 //      content: object,
 //      label: "string",
 //      status: DisplayStatus.enum,
 //      handler: handlerFunc,
-//      image: { url: "url", label: "string" } 
+//      image: { url: "url", label: "string" }
 // }
 const Carousel = props => {
-    const { className, items } = props;
+	const { className, items } = props;
 
-    const splideOptions = Object.freeze({
-        rewind: true,
-        autoWidth: true,
-        perPage: 2,
-    });
+	const splideOptions = Object.freeze({
+		rewind: true,
+		autoWidth: true,
+		perPage: 2
+	});
 
-    const classes = className ? `simple-carousel ${className}` : 'simple-carousel';
+	const classes = className
+		? `simple-carousel ${className}`
+		: 'simple-carousel';
 
-    return(
-        <Splide className={classes} options={splideOptions}>
-            { items.map(item => 
-                <SplideSlide key={item.id}>
-                    <ImageCard 
-                        id={item.id}
-                        label={item.label}
-                        status={item.status}
-                        ctaHandler={item.handler}
-                    >
-                        <img src={item.image.url} alt={item.label} />
-                    </ImageCard>
-                </SplideSlide>
-            )}
-        </Splide>
-    );
+	if (!items) {
+		return <Splide className={classes}></Splide>;
+	}
+
+	return (
+		<Splide
+			className={classes}
+			options={splideOptions}
+		>
+			{items.map(item => (
+				<SplideSlide key={item.id}>
+					<ImageCard
+						id={item.id}
+						label={item.label}
+						status={item.status}
+						ctaHandler={item.handler}
+					>
+						<img
+							src={item.image.url}
+							alt={item.label}
+						/>
+					</ImageCard>
+				</SplideSlide>
+			))}
+		</Splide>
+	);
 };
 
 export default Carousel;
--- meal-ui/src/pages/Home/View.js
+++ meal-ui/src/pages/Home/View.js
@@ -1,17 +1,12 @@
 import { useContext } from 'react';
-import Card, { SimpleCard } from 'components/Card';
-import Carousel from 'components/Carousel';
 import { GenericContext } from 'pages/GenericPageContainer';
-import Button from 'components/Button';
-import { useNavigate } from 'react-router-dom';
-import AppRoutes from 'navigation/AppRoutes';
+import { CommonActions } from './CommonActions';
+import { QuickRecipesView } from './QuickRecipesView';
 
 export default function View(props) {
-	const { plannedItems, suggestedItems } = props;
-
 	const genericContext = useContext(GenericContext);
 
-	const navigate = useNavigate();
+	const { plannedItems, suggestedItems } = props;
 
 	const classes = genericContext.className
 		? `p-home-view ${genericContext.className}`
@@ -19,49 +14,11 @@ export default function View(props) {
 
 	return (
 		<div className={classes}>
-			<Card
-				className='planning-quick-view'
-				title='Upcoming...'
-			>
-				<Carousel items={plannedItems} />
-			</Card>
-			<Card
-				className='explore-quick-view'
-				title='Something else?'
-			>
-				<Carousel items={suggestedItems} />
-			</Card>
-			<SimpleCard>
-				<div>
-					Recipes
-					<div className='action-option'>
-						Add a new recipe
-						<Button
-							handler={() => navigate(`${AppRoutes.recipe}/add`)}
-						>
-							Add
-						</Button>
-					</div>
-				</div>
-				<div>
-					Pantry
-					<div className='action-option'>
-						Update the pantry
-						<Button handler={() => navigate(`${AppRoutes.root}`)}>
-							add ingredients
-						</Button>
-					</div>
-				</div>
-				<div>
-					Planning
-					<div className='action-option'>
-						Create a meal plan
-						<Button handler={() => navigate(`${AppRoutes.root}`)}>
-							plan
-						</Button>
-					</div>
-				</div>
-			</SimpleCard>
+			<QuickRecipesView
+				plannedRecipes={plannedItems}
+				suggestedRecipes={suggestedItems}
+			/>
+			<CommonActions />
 		</div>
 	);
 }
