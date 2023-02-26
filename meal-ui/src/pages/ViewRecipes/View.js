b06e4da28ac6cbb4ec72f8ed197bdf936216c143
theQuantumMechanic <albertferguson118@gmail.com>
2023-02-26 17:28:11 +1100

    manage recipes page grid
    * scrollable
    
    * added a dummy header
    
    * spacing fixed up

--- meal-ui/src/pages/ViewRecipes/View.js
+++ meal-ui/src/pages/ViewRecipes/View.js
@@ -2,12 +2,33 @@ import { RecipeCard } from './RecipeCard';
 
 export default function View({ recipes }) {
 	return (
-		<div className='p-page scrollbar-vertical'>
-			<div className='recipe-grid'>
-				{recipes.map(r => (
-					<RecipeCard recipe={r} />
-				))}
+		<div className='p-manage-recipes'>
+			<ManageRecipesTable recipes={recipes} />
+		</div>
+	);
+}
+
+function ManageRecipesTable({ recipes }) {
+	//
+	return (
+		<div className='manage-recipes'>
+			<div className='grid-header font-white-header'>
+				<div>A</div>
+				<div>B</div>
+				<div>C</div>
 			</div>
+
+			<RecipeGrid recipes={recipes} />
+		</div>
+	);
+}
+
+function RecipeGrid({ recipes }) {
+	return (
+		<div className='recipes-grid scrollbar-vertical'>
+			{recipes.map(r => (
+				<RecipeCard recipe={r} />
+			))}
 		</div>
 	);
 }
