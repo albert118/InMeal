ddf2467a85c358188657d3d2e049a093d1b7c7be
theQuantumMechanic <albertferguson118@gmail.com>
2023-04-01 16:52:02 +1100

    install and add config for Next UI
    * adds _app.js
    * adds _document.js

--- meal-ui/src/App.js
+++ meal-ui/src/App.js
@@ -1,13 +1,16 @@
 import 'styles/styles.css';
 import RouterConfig from './navigation/RouterConfig';
 import { BrowserRouter } from 'react-router-dom';
+import { NextUIProvider } from '@nextui-org/react';
 
 export default function App() {
 	return (
 		<div className='meal-ui-basekit-theme negative-space-scale'>
-			<BrowserRouter>
-				<RouterConfig />
-			</BrowserRouter>
+			<NextUIProvider>
+				<BrowserRouter>
+					<RouterConfig />
+				</BrowserRouter>
+			</NextUIProvider>
 		</div>
 	);
 }
