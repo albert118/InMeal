17bb55eeb6814f76cfbcbfeb143e2eee40982e8a
theQuantumMechanic <albertferguson118@gmail.com>
2023-04-01 22:07:40 +1100

    neaten up card

--- meal-ui/src/components/Card/Card.js
+++ meal-ui/src/components/Card/Card.js
@@ -1,22 +1,13 @@
 import TitleBar from 'components/TitleBar/TitleBar';
 import React from 'react';
 
-
-const Card = props =>  {
-    const { className, title } = props;
-
-    const classes = className ? `card ${className}` : `card`;
-
-    return (
-        <div className={classes}>
-            <TitleBar>
-                {title}
-            </TitleBar>
-            <div className="content-slot">
-                {props.children}
-            </div>
-        </div>
-    );
+const Card = ({ className, title, children }) => {
+	return (
+		<div className={className ? `card ${className}` : `card`}>
+			<TitleBar>{title}</TitleBar>
+			<div className='content-slot'>{children}</div>
+		</div>
+	);
 };
 
 export default Card;
