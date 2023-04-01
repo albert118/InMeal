f0b3f4ace9508ebd13e242b80f1a9c795deaca90
theQuantumMechanic <albertferguson118@gmail.com>
2023-04-01 23:11:25 +1100

    fix card padding

--- meal-ui/src/components/Card/Card.js
+++ meal-ui/src/components/Card/Card.js
@@ -3,7 +3,11 @@ import { Tile } from '@carbon/react';
 
 const Card = ({ className, title, children }) => {
 	return (
-		<Tile className={className ? `card ${className}` : `card`}>
+		<Tile
+			className={
+				className ? `card common-card ${className}` : `card common-card`
+			}
+		>
 			<TitleBar>{title}</TitleBar>
 			<div className='content-slot'>{children}</div>
 		</Tile>
