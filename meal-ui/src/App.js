46d18d2444ad322a266749d5f029568a0a3d59aa
theQuantumMechanic <albertferguson118@gmail.com>
2023-04-01 17:21:52 +1100

    added custom theme

--- meal-ui/src/App.js
+++ meal-ui/src/App.js
@@ -2,11 +2,12 @@ import 'styles/styles.css';
 import RouterConfig from './navigation/RouterConfig';
 import { BrowserRouter } from 'react-router-dom';
 import { NextUIProvider } from '@nextui-org/react';
+import { mealUiTheme } from 'ThemeProvider';
 
 export default function App() {
 	return (
 		<div className='meal-ui-basekit-theme negative-space-scale'>
-			<NextUIProvider>
+			<NextUIProvider them={mealUiTheme}>
 				<BrowserRouter>
 					<RouterConfig />
 				</BrowserRouter>
---
+++ meal-ui/src/ThemeProvider.js
@@ -0,0 +1,23 @@
+import { createTheme } from '@nextui-org/react';
+
+export const mealUiTheme = createTheme({
+	type: 'dark',
+	theme: {
+		primaryLight: '--dove-grey',
+		primaryLightHover: '--dove-grey',
+		primaryLightActive: '--dove-grey',
+		primary: '--lynx-white',
+		primaryBorder: '--lynx-white',
+		primaryBorderHover: '--lynx-white',
+		primarySolidHover: '--lynx-white',
+		primarySolidContrast: '--lynx-white',
+		primaryShadow: '--lynx-white',
+
+		gradient:
+			'linear-gradient(to right, var(--lynx-white), rgba(255,0,0,0))',
+
+		link: '--note'
+	},
+	space: {},
+	fonts: {}
+});
