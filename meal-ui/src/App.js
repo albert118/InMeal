8ed611c7a9732c2ee74ce33b61e29eb740faddca
theQuantumMechanic <albertferguson118@gmail.com>
2023-04-01 19:32:05 +1100

    fix font import

--- meal-ui/src/App.js
+++ meal-ui/src/App.js
@@ -2,6 +2,9 @@ import 'styles/styles.css';
 import RouterConfig from './navigation/RouterConfig';
 import { BrowserRouter } from 'react-router-dom';
 
+/* import and use the recommended JoyUI font */
+import '@fontsource/public-sans';
+
 export default function App() {
 	return (
 		<div className='meal-ui-basekit-theme negative-space-scale'>
