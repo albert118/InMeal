d321fb97fc225feea27c1b4979efcede741dce1b
theQuantumMechanic <albertferguson118@gmail.com>
2023-04-02 00:12:32 +1100

    added a layout and cleaned up the footer

--- meal-ui/src/pages/GenericPageContainer.js
+++ meal-ui/src/pages/GenericPageContainer.js
@@ -1,7 +1,7 @@
 import { createContext } from 'react';
 import MinimalistSidebar from 'components/MinimalistSidebar';
 import Footer from 'components/Navigation';
-
+import { Grid, Column } from '@carbon/react';
 // create a generic context to inject data to children
 const GenericContext = createContext();
 
@@ -11,10 +11,28 @@ const GenericPageContainer = ({ className, children }) => {
 	return (
 		<div className={className ? `p-page ${className}` : `p-page`}>
 			<MinimalistSidebar />
-			<GenericContext.Provider value={genericContextProps}>
-				{children}
-			</GenericContext.Provider>
-			<Footer className='footer-grid' />
+			<Grid className='layout'>
+				<Column
+					className='layout__content'
+					xlg={16}
+					lg={16}
+					md={8}
+					sm={4}
+				>
+					<GenericContext.Provider value={genericContextProps}>
+						{children}
+					</GenericContext.Provider>
+				</Column>
+				<Column
+					className='layout__footer'
+					xlg={16}
+					lg={16}
+					md={8}
+					sm={4}
+				>
+					<Footer className='footer-grid' />
+				</Column>
+			</Grid>
 		</div>
 	);
 };
