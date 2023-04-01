0ce6f1102b2fe9734d0d24cd2059ca541cc8f28e
theQuantumMechanic <albertferguson118@gmail.com>
2023-04-01 19:23:44 +1100

    eh won't work

--- meal-ui/src/App.js
+++ meal-ui/src/App.js
@@ -2,16 +2,24 @@ import 'styles/styles.css';
 import RouterConfig from './navigation/RouterConfig';
 import { BrowserRouter } from 'react-router-dom';
 import { NextUIProvider } from '@nextui-org/react';
-import { mealUiTheme } from 'ThemeProvider';
+import { mealUiDarkTheme, lightModernTheme } from 'styles/ThemeProvider';
 
 export default function App() {
 	return (
 		<div className='meal-ui-basekit-theme negative-space-scale'>
-			<NextUIProvider them={mealUiTheme}>
+			<ConfiguredProvider>
 				<BrowserRouter>
 					<RouterConfig />
 				</BrowserRouter>
-			</NextUIProvider>
+			</ConfiguredProvider>
 		</div>
+	);
+}
+
+function ConfiguredProvider(props) {
+	return (
+		<NextUIProvider heme={lightModernTheme}>
+			{props.children}
+		</NextUIProvider>
 	);
 }
--- meal-ui/src/styles/ThemeProvider.js
+++ meal-ui/src/styles/ThemeProvider.js
@@ -1,24 +1,34 @@
 import { createTheme } from '@nextui-org/react';
 
-export const mealUiTheme = createTheme({
+export const mealUiDarkTheme = createTheme({
 	type: 'dark',
 	theme: {
-		// brand colours
-		primaryLight: '--dove-grey',
-		primaryLightHover: '--dove-grey',
-		primaryLightActive: '--dove-grey',
-		primary: '--lynx-white',
-		primaryBorder: '--lynx-white',
-		primaryBorderHover: '--lynx-white',
-		primarySolidHover: '--lynx-white',
-		primarySolidContrast: '--lynx-white',
-		primaryShadow: '--lynx-white',
+		colors: {
+			// // brand colours
+			primaryDark: 'var(--dove-grey)',
+			primaryLightHover: '--dove-grey',
+			primaryLightActive: '--dove-grey',
+			nextUIColorsPrimary: '--lynx-white',
+			primaryBorder: '--lynx-white',
+			primaryBorderHover: '--lynx-white',
+			primarySolidHover: '--lynx-white',
+			primarySolidContrast: '--lynx-white',
+			primaryShadow: '--lynx-white',
+			gradient:
+				'linear-gradient(to right, var(--lynx-white), rgba(255,0,0,0))',
+			link: '--note'
+		}
+	}
+});
 
-		gradient:
-			'linear-gradient(to right, var(--lynx-white), rgba(255,0,0,0))',
-
-		link: '--note'
-	},
-	space: {},
-	fonts: {}
+export const lightModernTheme = createTheme({
+	type: 'light',
+	className: 'light-modern',
+	theme: {
+		colors: {
+			primary: '#7928CA',
+			primaryLight: '#d9c2f0',
+			success: '#FF1CF7'
+		}
+	}
 });
