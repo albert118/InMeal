5b2b95fc32cb47a9a0143d5a3e1a797b1814bf53
theQuantumMechanic <albertferguson118@gmail.com>
2023-06-02 01:55:14 +1000

    remove console log

--- meal-ui/src/forms/Inputs/MultiSelectWithMultiLine.js
+++ meal-ui/src/forms/Inputs/MultiSelectWithMultiLine.js
@@ -31,7 +31,6 @@ export default function MultiSelectWithMultiLine({
 
 	const getSelectableItems = () => {
 		return objectMap(items, (key, value) => {
-			console.log(value);
 			return { id: key, label: value.label };
 		});
 	};
