fb0ac6a50740d8a38475fb55250891b2137427df
theQuantumMechanic <albertferguson118@gmail.com>
2023-06-05 00:37:51 +1000

    fix prop bug

--- meal-ui/src/dataHooks/useUpcomingRecipes.js
+++ meal-ui/src/dataHooks/useUpcomingRecipes.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import defaultRequestOptions from './defaultRequestOptions';
 import { ApiConfig } from 'config';
 
-export default function useUpcomingRecipes() {
+export default function useUpcomingRecipes(mapper) {
 	const [upcomingRecipes, setUpcomingRecipes] = useState([]);
 	const [isLoading, setLoading] = useState(false);
 	const [errors, setErrors] = useState(null);
@@ -11,7 +11,7 @@ export default function useUpcomingRecipes() {
 		fetchUpcomingRecipes();
 	}, []);
 
-	const fetchUpcomingRecipes = async mapper => {
+	const fetchUpcomingRecipes = async () => {
 		const url = `${ApiConfig.API_URL}/upcoming`;
 
 		setLoading(true);
