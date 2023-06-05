aad199109a435ccbff88fd12d9f3d8c515012de6
theQuantumMechanic <albertferguson118@gmail.com>
2023-06-06 00:10:32 +1000

    added validation error component

---
+++ meal-ui/src/forms/Recipe/components/ValidationErrors.jsx
@@ -0,0 +1,10 @@
+export default function ValidationErrors({ errors }) {
+	// don't render if there's nothing to show
+	const showErrors = errors && errors.length > 0;
+
+	const classes = `validation-errors ${
+		showErrors ? 'validation-error--active' : ''
+	}`;
+
+	return <div className={classes}>{errors && errors.map(e => <>{e}</>)}</div>;
+}
