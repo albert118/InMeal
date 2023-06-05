2bfdec841fba895902b27cf7cc49c4367af7e6d9
theQuantumMechanic <albertferguson118@gmail.com>
2023-06-05 22:11:42 +1000

    refactoring components

--- meal-ui/src/components/TitleBar/TitleBar.jsx
+++ meal-ui/src/components/TitleBar/TitleBar.jsx
@@ -2,17 +2,13 @@ import React from 'react';
 import Button from 'components/Button';
 
 /// Titlebar with an optional button!
-const TitleBar = props => {
-	const { className, btnText, handler } = props;
-
+export default function TitleBar({ className, btnText, handler, children }) {
 	const classes = className ? `title-bar ${className}` : `title-bar`;
 
 	return (
 		<div className={classes}>
-			<h2 className='title'>{props.children}</h2>
+			<h2 className='title'>{children}</h2>
 			{!!btnText && <Button onClick={handler}>{btnText}</Button>}
 		</div>
 	);
-};
-
-export default TitleBar;
+}
