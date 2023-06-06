46deb42b921ccd590f113efa5d1c31f41fd267ce
theQuantumMechanic <albertferguson118@gmail.com>
2023-06-07 01:18:49 +1000

    get rid of that manual delay bs (such a bad idea)

--- meal-ui/src/pages/useLayoutContext.jsx
+++ meal-ui/src/pages/useLayoutContext.jsx
@@ -1,8 +1,6 @@
 import { useState, useMemo, useCallback } from 'react';
 import { DefaultLayoutContext } from 'types/DefaultLayoutContext';
 
-const NOFLICKERLOADINGDELAY_MS = 300;
-
 /**
  * Create a layout context that child pages/components can share
  * This primarily shares the loading state management across multiple pages, allowing them to hook into a common page-loading spinner
@@ -12,12 +10,7 @@ const NOFLICKERLOADINGDELAY_MS = 300;
 export function useLayoutContext() {
 	const [layoutState, setLayoutState] = useState(DefaultLayoutContext);
 
-	const setIsLoading = useCallback(flag => {
-		// create a small delay to avoid "instant" loads or fast toggles flickering the screen
-		// this context and method are intended for use on the container level per page, individual or non-blocking API calls
-		// should avoid hooking into this and move as-fast-as-possible
-		setTimeout(() => {}, NOFLICKERLOADINGDELAY_MS);
-		console.log(`set isLoading '${flag}' from '${layoutState.isLoading}'`);
+	const setIsLoading = useCallback(async flag => {
 		setLayoutState({ ...DefaultLayoutContext, isLoading: flag });
 	}, []);
 
