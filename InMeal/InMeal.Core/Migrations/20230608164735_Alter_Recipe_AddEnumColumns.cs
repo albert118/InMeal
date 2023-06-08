56f36866ba6f4110ae724a9d34d45cfcb05e33a5
theQuantumMechanic <albertferguson118@gmail.com>
2023-06-09 02:50:10 +1000

    added enum columns to Recipe table

---
+++ InMeal/InMeal.Core/Migrations/20230608164735_Alter_Recipe_AddEnumColumns.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InMeal.Core.Migrations
+{
+    public partial class Alter_Recipe_AddEnumColumns : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CourseType",
+                table: "Recipe",
+                type: "longtext",
+                nullable: false,
+                defaultValue: "Unknown")
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "MealType",
+                table: "Recipe",
+                type: "longtext",
+                nullable: false,
+                defaultValue: "Unknown")
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CourseType",
+                table: "Recipe");
+
+            migrationBuilder.DropColumn(
+                name: "MealType",
+                table: "Recipe");
+        }
+    }
+}
