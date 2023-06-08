0a7bb355e8928038a557a0d4cd1980d4e11c84f3
theQuantumMechanic <albertferguson118@gmail.com>
2023-06-09 02:03:01 +1000

    added enum value converters

---
+++ InMeal/InMeal.Core/Globalisation/Converters/ModelConfigurationBuilderExtensions.cs
@@ -0,0 +1,16 @@
+﻿using InMeal.Core.Enumerations;
+using Microsoft.EntityFrameworkCore;
+
+namespace InMeal.Core.Globalisation.Converters;
+
+public static class ModelConfigurationBuilderExtensions
+{
+    public static ModelConfigurationBuilder AddEnumConversions(this ModelConfigurationBuilder builder)
+    {
+        builder.Properties<MealCourse>().HaveConversion<MealCourseConverter>();
+        builder.Properties<MealType>().HaveConversion<MealTypeConverter>();
+        builder.Properties<Cuisine>().HaveConversion<CuisineConverter>();
+
+        return builder;
+    }
+}
