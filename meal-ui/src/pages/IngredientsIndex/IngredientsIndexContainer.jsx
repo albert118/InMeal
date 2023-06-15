b8d5759d5c3c2e06452d4a60d7f4085e6c0ae3da
Albert Ferguson <albertferguson118@gmail.com>
2023-06-16 03:08:42 +1000

    shifted data concern to container

--- meal-ui/src/pages/IngredientsIndex/IngredientsIndexContainer.jsx
+++ meal-ui/src/pages/IngredientsIndex/IngredientsIndexContainer.jsx
@@ -5,9 +5,17 @@ import View from './View';
 export default function IngredientsIndexContainer() {
     const { setIsLoading } = useContext(LayoutContext);
 
+    const sampleIngredients = [
+        { name: 'salt', count: 7 },
+        { name: 'green onion', count: 7 },
+        { name: 'shallots', count: 7 },
+        { name: 'eggs', count: null },
+        { name: 'butter', count: 0 },
+    ];
+
     useEffect(() => {
 		setIsLoading(false);
 	}, []);
 
-    return <View />
+    return <View ingredients={sampleIngredients} />
 }
--- meal-ui/src/pages/IngredientsIndex/View.jsx
+++ meal-ui/src/pages/IngredientsIndex/View.jsx
@@ -1,17 +1,9 @@
 import { IngredientBadge } from './components';
 
-export default function View() {
-    const sampleIngredients = [
-        { name: 'salt', count: 7 },
-        { name: 'green onion', count: 7 },
-        { name: 'shallots', count: 7 },
-        { name: 'eggs', count: null },
-        { name: 'butter', count: 0 },
-    ];
-
+export default function View({ ingredients }) {
     return (
         <div className="simple-container">
-            {sampleIngredients.map(sample => {
+            {ingredients.map(sample => {
                 return <IngredientBadge
                     ingredientName={sample.name}
                     recipeUsagesCount={sample.count}
