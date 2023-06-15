a9666ac2787e54206243d6b5f31423059c5f9fbb
Albert Ferguson <albertferguson118@gmail.com>
2023-06-16 03:37:45 +1000

    comments

--- meal-ui/src/pages/IngredientsIndex/View.jsx
+++ meal-ui/src/pages/IngredientsIndex/View.jsx
@@ -1,6 +1,10 @@
 import { IngredientsIndexRow } from './components';
 import { objectMap } from 'utils';
 
+// TODO: wrap index rows into a container with divider styling and flesh 
+// out the index label
+// TODO: onClick modal
+// TODO: load real data (extend API to return the keyed dictionary)
 export default function View({ indexedIngredients }) {
     return (
         <div className="simple-container">
