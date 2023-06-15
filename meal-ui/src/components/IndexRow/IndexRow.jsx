66ce53ab853ccbfef6ef3020d7535085c65e9357
Albert Ferguson <albertferguson118@gmail.com>
2023-06-16 03:36:38 +1000

    tweaked index row design

--- meal-ui/src/components/IndexRow/IndexRow.jsx
+++ meal-ui/src/components/IndexRow/IndexRow.jsx
@@ -2,7 +2,9 @@ export default function IndexRow({ label, children }) {
     return (
         <div className="index-row">
             <label>{label}</label>
+            <div className="index-row--items">
             {children}
+            </div>
         </div>
     );
 }
