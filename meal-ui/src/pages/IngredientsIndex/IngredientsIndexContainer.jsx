ada6b8effdeff11b6dee1a69ef6e16a6de7e3bc9
Albert Ferguson <albertferguson118@gmail.com>
2023-06-16 03:29:20 +1000

    testing code

--- meal-ui/src/pages/IngredientsIndex/IngredientsIndexContainer.jsx
+++ meal-ui/src/pages/IngredientsIndex/IngredientsIndexContainer.jsx
@@ -6,10 +6,10 @@ export default function IngredientsIndexContainer() {
     const { setIsLoading } = useContext(LayoutContext);
 
     const sampleIngredients = {
-        0: [{ name: '12 things', count: 0 }, { name: '@#$ typo', count: 0 }],
-        1: [{ name: 'anchovies', count: 1 }, { name: 'avocado', count: 2 }, { name: 'asparagus', count: 4 }],
-        2: [{ name: 'butter', count: 7 }, { name: 'berries', count: 17 }, { name: 'baking soda', count: 2 }],
-        3: [{ name: 'coconut', count: 1 }, { name: 'chocolate', count: 3 }, { name: 'celery', count: 1 }]
+        '#': [{ name: '12 things', count: 0 }, { name: '@#$ typo', count: 0 }],
+        'A': [{ name: 'anchovies', count: 1 }, { name: 'avocado', count: 2 }, { name: 'asparagus', count: 4 }],
+        'B': [{ name: 'butter', count: 7 }, { name: 'berries', count: 17 }, { name: 'baking soda', count: 2 }],
+        'C': [{ name: 'coconut', count: 1 }, { name: 'chocolate', count: 3 }, { name: 'celery', count: 1 }]
     };
 
     useEffect(() => {
