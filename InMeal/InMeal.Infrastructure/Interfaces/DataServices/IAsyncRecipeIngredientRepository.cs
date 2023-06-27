50cc596859fc330392f9998f59dd61244dd045db
Albert Ferguson <albertferguson118@gmail.com>
2023-06-27 18:27:11 +1000

    implemented GetIngredientUsageCount on RecipeIngredient repo

--- InMeal/InMeal.Infrastructure/Interfaces/DataServices/IAsyncRecipeIngredientRepository.cs
+++ InMeal/InMeal.Infrastructure/Interfaces/DataServices/IAsyncRecipeIngredientRepository.cs
@@ -5,4 +5,6 @@ namespace InMeal.Infrastructure.Interfaces.DataServices;
 public interface IAsyncRecipeIngredientRepository
 {
     Task<List<RecipeIngredient>> GetRecipeIngredientsAsync(Guid recipeId, CancellationToken ct);
+
+    Task<Dictionary<Guid, int>> GetIngredientUsageCount(CancellationToken ct);
 }
