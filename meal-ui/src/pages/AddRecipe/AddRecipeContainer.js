4c7ff0973bf371d1b1f266d0dc90afa4bcd08382
Albert Ferguson <albertferguson118@gmail.com>
2023-07-17 23:05:03 +1000

    simplify forms
    * removed await syntax
    * simplified containers
    * fixed race condition

--- meal-ui/src/pages/AddRecipe/AddRecipeContainer.js
+++ meal-ui/src/pages/AddRecipe/AddRecipeContainer.js
@@ -1,15 +1,8 @@
 import View from './View';
-import { useIngredients, useRecipe } from 'hooks/data';
+import { useIngredients } from 'hooks/data';
 
 export default function AddRecipeContainer() {
 	const { ingredients } = useIngredients();
-	const { postEditedRecipe, postRecipe } = useRecipe(undefined);
 
-	return (
-		<View
-			ingredientOptions={ingredients}
-			postEditedRecipe={postEditedRecipe}
-			postRecipe={postRecipe}
-		/>
-	);
+	return <View ingredientOptions={ingredients} />;
 }
--- meal-ui/src/pages/EditRecipe/EditRecipeContainer.js
+++ meal-ui/src/pages/EditRecipe/EditRecipeContainer.js
@@ -1,17 +1,8 @@
 import View from './View';
-import { useParams } from 'react-router-dom';
-import { useRecipe, useIngredients } from 'hooks/data';
+import { useIngredients } from 'hooks/data';
 
 export default function EditRecipeContainer() {
-	const { recipeId } = useParams();
-	const { postEditedRecipe, recipe } = useRecipe(recipeId);
 	const { ingredients } = useIngredients();
 
-	return (
-		<View
-			existingRecipe={recipe}
-			ingredientOptions={ingredients}
-			postEditedRecipe={postEditedRecipe}
-		/>
-	);
+	return <View ingredientOptions={ingredients} />;
 }
