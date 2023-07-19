b1337ff2c033026761254755db7cf3f1398fbd9e
Albert Ferguson <albertferguson118@gmail.com>
2023-07-20 01:21:06 +1000

    simplify some view - containers (no need for view)

--- meal-ui/src/pages/EditRecipe/EditRecipeContainer.js
+++ meal-ui/src/pages/EditRecipe/EditRecipeContainer.js
@@ -1,12 +1,8 @@
-// import { HomeButton } from 'components/Button';
 import { AddOrEdit } from 'forms/Recipe';
 
 export default function EditRecipeContainer() {
 	return (
 		<div className='p-recipe'>
-			{/* <div className='left-panel'>
-		<HomeButton />
-	</div> */}
 			<AddOrEdit />
 		</div>
 	);
