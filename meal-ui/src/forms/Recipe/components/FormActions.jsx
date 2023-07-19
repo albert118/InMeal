1e2f7041da89909d339d1da3ca2dc1342e821133
Albert Ferguson <albertferguson118@gmail.com>
2023-07-20 01:15:50 +1000

    neaten up actions

--- meal-ui/src/forms/Recipe/components/FormActions.jsx
+++ meal-ui/src/forms/Recipe/components/FormActions.jsx
@@ -1,8 +1,10 @@
 import { CancelButton, SaveButton } from 'forms/FormActions';
+import ViewRecipeButton from './ViewRecipeButton';
 
-export default function FormActions({ handleCancel, saveActionText }) {
+export default function FormActions({ showViewButton, recipeId, handleCancel, saveActionText }) {
 	return (
 		<div className='action-container'>
+			{showViewButton && <ViewRecipeButton recipeId={recipeId} />}
 			<CancelButton onClick={handleCancel} />
 			<SaveButton>{saveActionText ? saveActionText : 'save'}</SaveButton>
 		</div>
