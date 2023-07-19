3be337e309a2f544c5383d6088a3722332468073
Albert Ferguson <albertferguson118@gmail.com>
2023-07-20 00:42:01 +1000

    removed home button on view page

--- meal-ui/src/pages/ViewRecipe/View.js
+++ meal-ui/src/pages/ViewRecipe/View.js
@@ -1,15 +1,10 @@
 import { TwoPaneRecipeCard } from 'components';
-import { HomeButton } from 'components/Button';
 
 export default function View(props) {
 	const { recipe } = props;
 
 	return (
 		<div className='p-recipe'>
-			{/* <div className='nav-actions'>
-				<HomeButton />
-			</div> */}
-
 			<TwoPaneRecipeCard recipe={recipe} />
 		</div>
 	);
