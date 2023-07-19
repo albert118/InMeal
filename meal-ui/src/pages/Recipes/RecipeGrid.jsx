6fa24712b199296b11d8577697115d7eaf975c62
Albert Ferguson <albertferguson118@gmail.com>
2023-07-20 01:59:14 +1000

    fix grid overflow

--- meal-ui/src/pages/Recipes/RecipeGrid.jsx
+++ meal-ui/src/pages/Recipes/RecipeGrid.jsx
@@ -2,7 +2,7 @@ import { SelectableRecipeCard } from 'components/RecipeCard';
 
 export function RecipeGrid({ recipes, addOrRemoveSelectedItem }) {
 	return (
-		<div className='recipes-grid'>
+		<div className='recipes-grid scrollbar-vertical'>
 			{recipes.map(recipe => (
 				<SelectableRecipeCard
 					key={recipe.id}
