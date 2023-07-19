3291daf0fac78533f25db51d4aea139fc9435884
Albert Ferguson <albertferguson118@gmail.com>
2023-07-20 01:16:06 +1000

    view - container simplified (removed view)

--- meal-ui/src/pages/EditRecipe/EditRecipeContainer.js
+++ meal-ui/src/pages/EditRecipe/EditRecipeContainer.js
@@ -1,5 +1,13 @@
-import View from './View';
+// import { HomeButton } from 'components/Button';
+import { AddOrEdit } from 'forms/Recipe';
 
 export default function EditRecipeContainer() {
-	return <View />;
+	return (
+		<div className='p-recipe'>
+			{/* <div className='left-panel'>
+		<HomeButton />
+	</div> */}
+			<AddOrEdit />
+		</div>
+	);
 }
