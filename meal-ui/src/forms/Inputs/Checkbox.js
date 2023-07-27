c24d7bacc18c58f05d94492dc891a9ff8e96d42b
Albert Ferguson <albertferguson118@gmail.com>
2023-07-27 14:54:38 +1000

    checkbox tweaked

--- meal-ui/src/forms/Inputs/Checkbox.js
+++ meal-ui/src/forms/Inputs/Checkbox.js
@@ -1,12 +1,18 @@
-export default function Checkbox({ label, className, ...additionalProps }) {
+export default function Checkbox({ label, className, disabled, ...additionalProps }) {
 	return (
 		<div className={`form-input u-form-flexed-input ${className ?? ''}`}>
 			<input
 				id={label}
 				type='checkbox'
+				disabled={disabled}
 				{...additionalProps}
 			/>
-			<label htmlFor={label}>{label}</label>
+			<label
+				className={`${disabled ? 'disabled' : ''}`}
+				htmlFor={label}
+			>
+				{label}
+			</label>
 		</div>
 	);
 }
