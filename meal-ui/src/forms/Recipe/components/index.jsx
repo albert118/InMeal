846f031834f33ce95bfd82636539885c81b37ecb
Albert Ferguson <albertferguson118@gmail.com>
2023-09-02 14:41:58 +1000

    removed button from index

--- meal-ui/src/forms/Recipe/components/index.jsx
+++ meal-ui/src/forms/Recipe/components/index.jsx
@@ -1,3 +1,2 @@
-export { default as ViewRecipeButton } from './ViewRecipeButton';
 export { default as FormActions } from './FormActions';
 export { default as MultiSelectItemBadge } from './MultiSelectItemBadge';
