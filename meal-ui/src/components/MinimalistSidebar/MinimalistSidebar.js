832636b7fb8796a7b3594338f7ad38170a3d152d
theQuantumMechanic <albertferguson118@gmail.com>
2023-09-02 16:57:40 +1000

    Feature/move add btn from homepage (moved to sidebar) (#77)

--- meal-ui/src/components/MinimalistSidebar/MinimalistSidebar.js
+++ meal-ui/src/components/MinimalistSidebar/MinimalistSidebar.js
@@ -1,5 +1,5 @@
 import { useNavigate } from 'react-router-dom';
-import { faSeedling, faBoxes } from '@fortawesome/free-solid-svg-icons';
+import { faSeedling, faBoxes, faSquarePlus } from '@fortawesome/free-solid-svg-icons';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import { AnimatedHamburger } from './AnimatedHamburger';
 import { NavLinkItem } from './NavLinkItem';
@@ -39,14 +39,21 @@ export default function MinimalistSidebar({ isActive, setActive, isInActive, set
 					icon={faSeedling}
 					onClick={() => navigate(AppRoutes.ingredients)}
 				>
-					View Ingredients
+					view ingredients
 				</NavLinkItem>
 				<NavLinkItem
 					isActive={isActive}
 					icon={faBoxes}
 					onClick={() => navigate(AppRoutes.recipes)}
 				>
-					Manage Recipes
+					manage Recipes
+				</NavLinkItem>
+				<NavLinkItem
+					isActive={isActive}
+					icon={faSquarePlus}
+					onClick={() => navigate(`${AppRoutes.recipe}/add`)}
+				>
+					add a new recipe
 				</NavLinkItem>
 			</div>
 			<div className='nav-links social-links'>
