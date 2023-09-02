46e1ad454e6284f412e6159d7b691f6fa9cef3a0
Albert Ferguson <albertferguson118@gmail.com>
2023-09-02 16:35:38 +1000

    only update if there are changes

--- meal-ui/src/forms/EditIngredient/useEditIngredientFormData.js
+++ meal-ui/src/forms/EditIngredient/useEditIngredientFormData.js
@@ -5,41 +5,51 @@ const defaultFormState = (name, units) => {
 	return {
 		name: name,
 		unit: units.name,
-		isDeleted: false
+		isDeleted: false,
+		hasUpdated: false
 	};
 };
 
-export default function useEditIngredientFormData(ingredient, refreshCallback) {
+export default function useEditIngredientFormData(ingredient) {
 	const [formData, setFormData] = useState(defaultFormState(ingredient.name, ingredient.units));
 	const [disableDelete, setDisableDelete] = useState(ingredient.recipeUsageCount !== 0);
 
 	const { updateIngredient, deleteIngredient } = useIngredient();
+
+	const valueUpdateStrategies = {
+		isDeleted: target => target.checked,
+		default: target => target.value
+	};
 
 	useEffect(() => {
 		setDisableDelete(ingredient.recipeUsageCount !== 0);
 	}, [ingredient]);
 
 	function onUpdate(event) {
+		const getTarget = valueUpdateStrategies[event.target.name] ?? valueUpdateStrategies['default'];
+
 		setFormData({
 			...formData,
-			[event.target.name]:
-				event.target.name === 'isDeleted' ? event.target.checked : event.target.value
+			[event.target.name]: getTarget(event.target),
+			hasUpdated: true
 		});
 	}
 
-	function onSubmit(event) {
-		event.preventDefault();
-
+	// no event to receive, as the modal isn't a form
+	// returns the fetch promise so the consumer can wait for a success/error response
+	function onSubmit() {
 		const shouldDelete = formData.isDeleted && ingredient.recipeUsageCount === 0;
 
 		if (shouldDelete) {
-			// TODO: ensure these callbacks work as expected with the datahook
-			deleteIngredient(ingredient.ingredientId).then(() => refreshCallback());
-		} else {
-			updateIngredient(ingredient.ingredientId, formData.name, formData.unit).then(() =>
-				refreshCallback()
-			);
+			return deleteIngredient(ingredient.ingredientId);
 		}
+
+		// dont post an update if no data has changed
+		if (formData.hasUpdated) {
+			return updateIngredient(ingredient.ingredientId, formData.name, formData.unit);
+		}
+
+		return Promise.resolve();
 	}
 
 	function getWarnings() {
