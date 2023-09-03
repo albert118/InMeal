afcacc786326299778a377aaff8c0ea862ee3152
Albert Ferguson <albertferguson118@gmail.com>
2023-09-03 18:11:26 +1000

    fixups

--- meal-ui/src/components/ItemBadge/ItemBadge.jsx
+++ meal-ui/src/components/ItemBadge/ItemBadge.jsx
@@ -1,8 +1,8 @@
 import { Badge } from 'components';
 
-export default function ItemBadge({ item, subLabel }) {
+export default function ItemBadge({ item, subLabel, className }) {
 	return (
-		<div className='e-multi-select-badge ingredient-form-row--ingredient-badge'>
+		<div className={`e-multi-select-badge ${className ?? ''}`}>
 			<Badge
 				id={item.id}
 				text={item.hasOwnProperty('label') ? item.label : item}
