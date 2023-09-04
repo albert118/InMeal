811d2aa9320048c2fde221766dca2cb01d85072a
Albert Ferguson <albertferguson118@gmail.com>
2023-09-04 20:46:32 +1000

    remove sticky background

--- meal-ui/src/pages/Recipes/ManageRecipesTable.jsx
+++ meal-ui/src/pages/Recipes/ManageRecipesTable.jsx
@@ -1,6 +1,4 @@
-import { useState } from 'react';
-import { Button } from 'components';
-import { Checkbox } from 'forms/Inputs';
+import { Button, Toggle } from 'components';
 import AppRoutes from 'navigation/AppRoutes';
 import { useNavigate } from 'react-router-dom';
 import { objectMap } from 'utils';
@@ -9,15 +7,12 @@ import useManagementTable from './useManagementTable';
 
 export function ManageRecipesTable() {
 	const { recipes, onAddOrRemove, onArchive, onViewArchived } = useManagementTable();
-	const [scroll, setScroll] = useState(false);
 
 	return (
-		// need to hack around this
-		<div onWheel={() => setScroll(!scroll)}>
+		<div>
 			<Actions
 				onArchive={onArchive}
 				onViewArchived={onViewArchived}
-				scroll={scroll}
 			/>
 			{objectMap(recipes, (group, recipes) => {
 				return (
@@ -33,38 +28,34 @@ export function ManageRecipesTable() {
 	);
 }
 
-function Actions({ onViewArchived, onArchive, scroll }) {
+function Actions({ onViewArchived, onArchive }) {
 	const navigate = useNavigate();
 
 	return (
 		<div className='action-container'>
-			{scroll && (
-				<div
-					className={`action-container__invisible-gutter ${
-						scroll ? 'action-container__invisible-gutter_active' : ''
-					}`}
-				/>
-			)}
-			<div className='action-container__actions'>
-				<Checkbox
-					className='font-white'
-					label='view archived'
-					onClick={onViewArchived}
-				/>
-				<Button
-					onClick={() => navigate(`${AppRoutes.recipe}/add`)}
-					kind='secondary'
-					className='add-recipes-btn'
-				>
-					add
-				</Button>
-				<Button
-					onClick={onArchive}
-					kind='secondary'
-					className='delete-recipes-btn'
-				>
-					archive
-				</Button>
+			<div className='action-container__card'>
+				<div className='filters'>
+					<Toggle
+						id='1'
+						className='font-white'
+						labelText='view archived'
+						onClick={onViewArchived}
+					/>
+				</div>
+				<div className='actions'>
+					<Button
+						onClick={() => navigate(`${AppRoutes.recipe}/add`)}
+						kind='secondary'
+					>
+						add
+					</Button>
+					<Button
+						onClick={onArchive}
+						kind='secondary'
+					>
+						archive
+					</Button>
+				</div>
 			</div>
 		</div>
 	);
