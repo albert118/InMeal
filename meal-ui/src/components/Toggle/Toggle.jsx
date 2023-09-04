4c1a2a53193b0a6a845f6314652f64a91b796d95
Albert Ferguson <albertferguson118@gmail.com>
2023-09-04 22:32:10 +1000

    fix toggle

--- meal-ui/src/components/Toggle/Toggle.jsx
+++ meal-ui/src/components/Toggle/Toggle.jsx
@@ -3,7 +3,7 @@ import { Toggle } from 'carbon-components-react';
 export default function ToggleCustom({ onClick, ...additionalProps }) {
 	const onClickAdaptor = event => {
 		return onClick({
-			target: { checked: !!event.target.ariaChecked, ...event.target }
+			target: { checked: event.target.ariaChecked !== 'true', ...event.target }
 		});
 	};
 
