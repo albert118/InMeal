be8c67b17692859ed2336656ee100a0ecf40c730
Albert Ferguson <albertferguson118@gmail.com>
2023-09-04 20:28:11 +1000

    WIP

--- meal-ui/src/pages/Recipes/ManageRecipesTable.jsx
+++ meal-ui/src/pages/Recipes/ManageRecipesTable.jsx
@@ -1,15 +1,23 @@
-import { Actions } from './Actions';
-import useManagementTable from './useManagementTable';
-import ManageRecipesRow from './ManageRecipesRow';
+import { useState } from 'react';
+import { Button } from 'components';
+import { Checkbox } from 'forms/Inputs';
+import AppRoutes from 'navigation/AppRoutes';
+import { useNavigate } from 'react-router-dom';
 import { objectMap } from 'utils';
+import ManageRecipesRow from './ManageRecipesRow';
+import useManagementTable from './useManagementTable';
 
 export function ManageRecipesTable() {
 	const { recipes, onAddOrRemove, onArchive, onViewArchived } = useManagementTable();
+	const [scroll, setScroll] = useState(false);
+
 	return (
-		<div>
+		// need to hack around this
+		<div onWheel={() => setScroll(!scroll)}>
 			<Actions
 				onArchive={onArchive}
 				onViewArchived={onViewArchived}
+				scroll={scroll}
 			/>
 			{objectMap(recipes, (group, recipes) => {
 				return (
@@ -21,6 +29,43 @@ export function ManageRecipesTable() {
 					/>
 				);
 			})}
+		</div>
+	);
+}
+
+function Actions({ onViewArchived, onArchive, scroll }) {
+	const navigate = useNavigate();
+
+	return (
+		<div className='action-container'>
+			{scroll && (
+				<div
+					className={`action-container__invisible-gutter ${
+						scroll ? 'action-container__invisible-gutter_active' : ''
+					}`}
+				/>
+			)}
+			<div className='action-container__actions'>
+				<Checkbox
+					className='font-white'
+					label='view archived'
+					onClick={onViewArchived}
+				/>
+				<Button
+					onClick={() => navigate(`${AppRoutes.recipe}/add`)}
+					kind='secondary'
+					className='add-recipes-btn'
+				>
+					add
+				</Button>
+				<Button
+					onClick={onArchive}
+					kind='secondary'
+					className='delete-recipes-btn'
+				>
+					archive
+				</Button>
+			</div>
 		</div>
 	);
 }
