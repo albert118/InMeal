c75f74ae908c5e1eb8612e3d4c9f7181d98c9a84
Albert Ferguson <albertferguson118@gmail.com>
2023-09-06 21:53:19 +1000

    no UUID

--- meal-ui/src/forms/Inputs/LongTextInput.js
+++ meal-ui/src/forms/Inputs/LongTextInput.js
@@ -3,7 +3,7 @@ export default function LongTextInput({ label, rows, ...additionalProps }) {
 		<div className={`form-input ${additionalProps.className ?? ''}`}>
 			{label ? <label htmlFor={label}>{label}</label> : ''}
 			<textarea
-				id={label ?? crypto.randomUUID()}
+				id={label ?? 'default_long_text_input_id'}
 				className='long-text-input scrollbar-vertical'
 				rows={rows ?? '4'}
 				maxLength='5000'
