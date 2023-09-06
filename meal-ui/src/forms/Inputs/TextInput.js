e8b26d131410726b9848856b13ee4c5dcf77d7cd
Albert Ferguson <albertferguson118@gmail.com>
2023-09-06 22:40:40 +1000

    remove old "handler"

--- meal-ui/src/forms/Inputs/TextInput.js
+++ meal-ui/src/forms/Inputs/TextInput.js
@@ -5,7 +5,6 @@ export default function TextInput({ label, onChange, handleKeyDown, ...additiona
 			<input
 				id={additionalProps.id ?? 'default_text_input_id'}
 				className='text-input'
-				onChange={handler}
 				onKeyDown={handleKeyDown}
 				type='text'
 			/>
