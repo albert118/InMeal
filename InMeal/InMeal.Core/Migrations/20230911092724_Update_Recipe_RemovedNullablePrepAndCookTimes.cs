b443b6f1b435d0c913696bab994a2d505e556963
Albert Ferguson <albertferguson118@gmail.com>
2023-09-11 19:28:12 +1000

    migrated change

---
+++ InMeal/InMeal.Core/Migrations/20230911092724_Update_Recipe_RemovedNullablePrepAndCookTimes.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InMeal.Core.Migrations
+{
+    public partial class Update_Recipe_RemovedNullablePrepAndCookTimes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "PrepTime",
+                table: "Recipe",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CookTime",
+                table: "Recipe",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "PrepTime",
+                table: "Recipe",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CookTime",
+                table: "Recipe",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+    }
+}
