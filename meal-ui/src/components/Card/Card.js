6f64bee0086fe0541e693c67bf0079f8427ef204
theQuantumMechanic <albertferguson118@gmail.com>
2023-09-21 16:44:01 +1000

    Bug/mobile homepage sizing (#86)

--- meal-ui/src/components/Card/Card.js
+++ meal-ui/src/components/Card/Card.js
@@ -4,9 +4,8 @@ import { Tile } from '@carbon/react';
 const Card = ({ className, title, children }) => {
 	return (
 		<Tile
-			className={
-				className ? `card common-card ${className}` : `card common-card`
-			}
+			style={{ padding: 0 }}
+			className={`card ${className ?? ''}`}
 		>
 			<TitleBar>{title}</TitleBar>
 			<div className='content-slot'>{children}</div>
