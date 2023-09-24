429f66e7676e8d83d2bfbd238422ff82f7a748b0
Albert Ferguson <albertferguson118@gmail.com>
2023-09-24 22:22:57 +1000

    bump version

--- meal-ui/src/VersionInfo.js
+++ meal-ui/src/VersionInfo.js
@@ -1,10 +1,10 @@
 const VersionInfo = {
-    major: 0,
-    minor: 1,
-    revision: 'alpha',
-    toString() { 
-        return `${this.major}.${this.minor}.${this.revision}` 
-    }
+	major: 1,
+	minor: 3,
+	revision: 3,
+	toString() {
+		return `${this.major}.${this.minor}.${this.revision}`;
+	}
 };
 
 export default Object.freeze(Object.create(VersionInfo));
