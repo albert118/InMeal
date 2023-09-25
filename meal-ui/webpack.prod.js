dcd25e0d4af783325c08ef3822b77d75e9791655
Albert Ferguson <albertferguson118@gmail.com>
2023-09-26 00:44:32 +1000

    notes for performance enhancements

--- meal-ui/webpack.prod.js
+++ meal-ui/webpack.prod.js
@@ -1,12 +1,15 @@
-const path = require('path');
-const webpack = require('webpack');
+const webpack = require('webpack')
 
+// TODO: look into suspense and dynamic imports
+// https://react.dev/reference/react/lazy#suspense-for-code-splitting
+// TODO: look at prefetching
+// https://webpack.js.org/guides/code-splitting/#prefetchingpreloading-modules
 module.exports = {
-	mode: 'production',
-	devtool: 'source-map',
-	plugins: [
-		new webpack.DefinePlugin({
-			'process.env.name': JSON.stringify('prod')
-		})
-	]
-};
+    mode: 'production',
+    devtool: 'source-map',
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env.name': JSON.stringify('prod'),
+        }),
+    ],
+}
