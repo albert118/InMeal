69ab3d83ed7c9021d434ea1008ec1f5af63b8d03
Albert Ferguson <albertferguson118@gmail.com>
2023-09-26 00:18:51 +1000

    added eslint + improved prettier configs

---
+++ meal-ui/.eslintrc.js
@@ -0,0 +1,51 @@
+// https://eslint.org/docs/latest/use/command-line-interface
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+    },
+    settings: {
+        react: {
+            version: 'detect',
+        },
+    },
+    extends: [
+        'plugin:react/recommended',
+        'plugin:react-hooks/recommended',
+        'plugin:import/errors',
+        'plugin:import/warnings',
+        'plugin:jsx-a11y/recommended',
+        'prettier',
+        'plugin:prettier/recommended',
+    ],
+    rules: {
+        'no-unused-vars': 'off',
+        'react/prop-types': 'off',
+        'react/jsx-uses-react': 'off',
+        'react/react-in-jsx-scope': 'off',
+    },
+    settings: {
+        // https://github.com/johvin/eslint-import-resolver-alias#readme
+        'import/resolver': {
+            alias: {
+                map: [
+                    ['assets', './src/assets'],
+                    ['components', './src/components'],
+                    ['config', './src/config'],
+                    ['forms', './src/forms'],
+                    ['hooks', './src/hooks'],
+                    ['navigation', './src/navigation'],
+                    ['pages', './src/pages'],
+                    ['styles', './src/styles'],
+                    ['types', './src/types'],
+                    ['utils', './src/utils'],
+                    ['VersionInfo', './src/VersionInfo.js'],
+                    // demo image for prototyping purposes
+                    ['DemoImage', './src/DemoImage.js'],
+                ],
+                extensions: ['.scss', '.js', '.jsx', '.json'],
+            },
+        },
+    },
+}
