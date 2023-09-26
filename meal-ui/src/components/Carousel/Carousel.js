e3952060c025cfd523befc3504ce17d17eea7d43
Albert Ferguson <albertferguson118@gmail.com>
2023-09-26 13:04:29 +1000

    fix return statement

--- meal-ui/src/components/Carousel/Carousel.js
+++ meal-ui/src/components/Carousel/Carousel.js
@@ -3,13 +3,13 @@ import { propagateProps } from 'utils'
 export default function Carousel({ items, ...additionalProps }) {
     return (
         <div className={`simple-carousel ${additionalProps.className ?? ''}`}>
-            {items?.map((item) => {
+            {items?.map((item) =>
                 propagateProps(additionalProps.children, {
                     key: item.id,
                     ...item,
                     className: 'carousel-item',
-                })
-            })}
+                }),
+            )}
         </div>
     )
 }
