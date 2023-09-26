e910080e3c3071062012a6bbda6357b18812ff2b
Albert Ferguson <albertferguson118@gmail.com>
2023-09-26 13:27:45 +1000

    nav buttons

--- meal-ui/src/components/Button/Button.js
+++ meal-ui/src/components/Button/Button.js
@@ -1,46 +1,60 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { Button } from '@carbon/react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { Button } from '@carbon/react'
 
 const CustomButton = ({ className, children, ...additionalProps }) => {
-	return (
-		<Button
-			{...additionalProps}
-			className={className ? `btn  ${className}` : `btn`}
-		>
-			{children}
-		</Button>
-	);
-};
+    return (
+        <Button
+            {...additionalProps}
+            className={className ? `btn  ${className}` : `btn`}
+        >
+            {children}
+        </Button>
+    )
+}
 
 const IconButton = ({ faIcon, isPrimary, ...additionalProps }) => {
-	return (
-		<Button
-			{...additionalProps}
-			className='icon-btn cds--btn--icon-only'
-			kind={isPrimary ? 'primary' : 'ghost'}
-			size='lg'
-		>
-			<FontAwesomeIcon icon={faIcon} />
-		</Button>
-	);
-};
+    return (
+        <Button
+            {...additionalProps}
+            className="icon-btn cds--btn--icon-only"
+            kind={isPrimary ? 'primary' : 'ghost'}
+            size="lg"
+        >
+            <FontAwesomeIcon icon={faIcon} />
+        </Button>
+    )
+}
 
 const LabelledIconButton = ({
-	faIcon,
-	isPrimary,
-	children,
-	...additionalProps
+    faIcon,
+    isPrimary,
+    children,
+    ...additionalProps
 }) => {
-	return (
-		<Button
-			{...additionalProps}
-			className='btn labelled-icon-btn'
-			kind={isPrimary ? 'primary' : 'ghost'}
-		>
-			<FontAwesomeIcon icon={faIcon} />
-			<span>{children}</span>
-		</Button>
-	);
-};
+    return (
+        <Button
+            {...additionalProps}
+            className="btn labelled-icon-btn"
+            kind={isPrimary ? 'primary' : 'ghost'}
+        >
+            <FontAwesomeIcon icon={faIcon} />
+            <span>{children}</span>
+        </Button>
+    )
+}
 
-export { CustomButton as Button, IconButton, LabelledIconButton };
+function NavigationArrowLeft({ ...additionalProps }) {
+    return <button className="btn nav-btn">&lt;</button>
+}
+
+function NavigationArrowRight({ ...additionalProps }) {
+    return <button className="btn nav-btn">&gt;</button>
+}
+
+export {
+    CustomButton as Button,
+    IconButton,
+    LabelledIconButton,
+    NavigationArrowLeft,
+    NavigationArrowRight,
+}
--- meal-ui/src/components/Button/index.js
+++ meal-ui/src/components/Button/index.js
@@ -1,5 +1,17 @@
-import { Button, LabelledIconButton, IconButton } from './Button';
-import HomeButton from './HomeButton';
+import {
+    Button,
+    LabelledIconButton,
+    IconButton,
+    NavigationArrowLeft,
+    NavigationArrowRight,
+} from './Button'
+import HomeButton from './HomeButton'
 
-export default Button;
-export { LabelledIconButton, IconButton, HomeButton };
+export default Button
+export {
+    LabelledIconButton,
+    IconButton,
+    HomeButton,
+    NavigationArrowLeft,
+    NavigationArrowRight,
+}
