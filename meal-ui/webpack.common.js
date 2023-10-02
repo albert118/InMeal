c6dcf7e232a20be548e12fbeaaf487d426860825
Albert Ferguson <albertferguson118@gmail.com>
2023-10-02 15:57:36 +1100

    ensure absoulte public path for assets

--- meal-ui/webpack.common.js
+++ meal-ui/webpack.common.js
@@ -5,6 +5,7 @@ module.exports = {
 	entry: path.resolve(__dirname, 'src/index.js'),
 	output: {
 		path: path.resolve(__dirname, 'dist'),
+		publicPath: '/',
 		filename: 'assets/js/[name].[contenthash:8].js',
 		clean: true
 	},
