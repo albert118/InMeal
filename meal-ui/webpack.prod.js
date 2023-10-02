482d671daa0a3187a4e04f2acf04a3722b24e71f
Albert Ferguson <albertferguson118@gmail.com>
2023-10-02 16:43:12 +1100

    supprt prod previewing and add minification

--- meal-ui/webpack.prod.js
+++ meal-ui/webpack.prod.js
@@ -1,4 +1,6 @@
-const webpack = require('webpack')
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 // TODO: look into suspense and dynamic imports
 // https://react.dev/reference/react/lazy#suspense-for-code-splitting
@@ -9,7 +11,24 @@ module.exports = {
     devtool: 'source-map',
     plugins: [
         new webpack.DefinePlugin({
-            'process.env.name': JSON.stringify('prod'),
+            'process.env.name': JSON.stringify('prod')
         }),
-    ],
-}
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'src/assets/index.html'),
+            favicon: path.resolve(__dirname, 'src/assets/favicon.ico'),
+            xhtml: true,
+            inject: true,
+            minify: {
+                removeComments: true,
+                collapseWhitespace: true,
+                removeRedundantAttributes: true,
+                useShortDoctype: true,
+                removeEmptyAttributes: true,
+                keepClosingSlash: true,
+                minifyJS: true,
+                minifyCSS: true,
+                minifyURLs: true
+            }
+        })
+    ]
+};
