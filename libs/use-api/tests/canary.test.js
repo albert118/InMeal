01a09cf15b801c667e6f51640f5009e9b7b996fe
Albert Ferguson <albertferguson118@gmail.com>
2024-06-12 00:06:48 +1000

    bootstrapped FUI testing

--- libs/use-api/tests/canary.test.js
+++ libs/use-api/tests/canary.test.js
@@ -1,4 +1,4 @@
-describe('canary in the coal mine (should always pass)', () => {
+describe('canary in the coal mine (should always pass) 🐦️', () => {
     test('this test should always pass no matter what', () => {
         const canary = { alive: true };
         expect(canary.alive).toBe(true);
