5cae93e8ccb37da951e04f059f453cabfb8b7987
Albert Ferguson <albertferguson118@gmail.com>
2024-06-12 01:13:09 +1000

    bump version for release

--- apps/fui/src/VersionInfo.js
+++ apps/fui/src/VersionInfo.js
@@ -1,7 +1,7 @@
 const VersionInfo = {
     major: 1,
-    minor: 3,
-    revision: 3,
+    minor: 6,
+    revision: 0,
     toString() {
         return `${this.major}.${this.minor}.${this.revision}`;
     }
