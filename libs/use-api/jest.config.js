61f02ce04dfa657cb216d3d35d0a010a75b0d7da
theQuantumMechanic <albertferguson118@gmail.com>
2024-06-12 00:27:51 +1000

    Infrastructure/bump nx to v18 and support Jest (#142)

---
+++ libs/use-api/jest.config.js
@@ -0,0 +1,10 @@
+/* eslint-disable */
+module.exports = {
+    displayName: 'use-api',
+    preset: '../../jest.preset.js',
+    transform: {
+        '^.+\\.[tj]s$': 'babel-jest'
+    },
+    moduleFileExtensions: ['ts', 'js'],
+    coverageDirectory: '../../coverage/libs/use-api'
+};
