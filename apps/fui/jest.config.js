e5351835138908b039d9446b31b320dcb837c4a1
Albert Ferguson <albertferguson118@gmail.com>
2024-06-12 00:07:36 +1000

    bootstrapped FUI testing

---
+++ apps/fui/jest.config.js
@@ -0,0 +1,10 @@
+/* eslint-disable */
+module.exports = {
+    displayName: '@inmeal/fui',
+    preset: '../../jest.preset.js',
+    transform: {
+        '^.+\\.[tj]s$': 'babel-jest'
+    },
+    moduleFileExtensions: ['ts', 'js', 'html'],
+    coverageDirectory: '../../coverage/apps/fui'
+};
