5baf0d95d248907e9346bf246204d6256b19a50e
Albert Ferguson <albertferguson118@gmail.com>
2024-06-12 01:03:43 +1000

    fixups for test config and extend canary test

--- apps/fui/src/canary.test.js
+++ apps/fui/src/canary.test.js
@@ -3,4 +3,11 @@ describe('canary in the coal mine (should always pass) 🐦️', () => {
         const canary = { alive: true };
         expect(canary.alive).toBe(true);
     });
+
+    test('FUI should resolve expected version', async () => {
+        // assert both that the environment is configured for module imports correctly
+        // AND that the current version is resolvable
+        const { default: VersionInfo } = await import('./VersionInfo.js');
+        expect(VersionInfo.toString()).toBe('1.3.3');
+    });
 });
--- libs/use-api/jest.config.js
+++ libs/use-api/jest.config.js
@@ -1,10 +1,5 @@
 /* eslint-disable */
 module.exports = {
     displayName: 'use-api',
-    preset: '../../jest.preset.js',
-    transform: {
-        '^.+\\.[tj]s$': 'babel-jest'
-    },
-    moduleFileExtensions: ['ts', 'js'],
-    coverageDirectory: '../../coverage/libs/use-api'
+    preset: '../../jest.preset.js'
 };
